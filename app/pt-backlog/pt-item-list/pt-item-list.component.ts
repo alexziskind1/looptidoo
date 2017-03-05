6dd69b3ddf6f299aaaead480e536f3ad547da7dd
Alexander Ziskind <alex@nuvious.com>
2017-03-05 18:29:34 -0500

    fixed up main list

--- app/pt-backlog/pt-item-list/pt-item-list.component.ts
+++ app/pt-backlog/pt-item-list/pt-item-list.component.ts
@@ -91,52 +91,25 @@ export class PTItemListComponent implements OnInit {
         private backlogService: BacklogService,
         private authService: AuthenticationService,
         private _routerExtensions: RouterExtensions) {
-        this.backlogService.ptItemsObs.subscribe(data => {
-            console.log('data returned');
-            console.dir(data);
-            this.ptItems = data;
-        });
-        console.log('list contrsuctor selectedViewIndex: ' + this.selectedViewIndex);
 
-        // this.page.on(Page.navigatingToEvent, () => {
-        //    this.backlogService.publishUpdates();
-        //});
     }
 
     public ngOnInit() {
-        console.log('list ngOnInit selectedViewInde: ' + this.selectedViewIndex);
+        this.backlogService.ptItemsObs.subscribe(data => {
+            this.ptItems = data;
+        });
     }
 
     private refresh() {
-        console.log('refresh');
         let filterState: FilterState = new FilterState(
             this.selectedViewIndex);
         this.backlogService.filter(filterState);
     }
 
-    public btnLoaded(args) {
-        console.log('btn loaded: ' + args);
-        var btn = args.object;
-        //TODO: check for specific platform
-        if (btn.android) {
-            btn.android.setFocusable(false);
-        }
-    }
-
     public listItemTap(args: ItemEventData) {
         let lv = <ListView>args.object;
         let item = <IPTItem>lv.items[args.index];
-        console.log('list item tap: ' + item.title);
-
         this._routerExtensions.navigate(['/pt-item', item.id]);
-
-        //this._routerExtensions.navigate()
-    }
-
-    public changeMeTapped(item: IPTItem) {
-        console.log(item.title);
-        this.backlogService.incrementEstimate(item);
-        this.backlogService.switchAssignee(item);
     }
 
 }
