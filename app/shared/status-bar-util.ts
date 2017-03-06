b6ba661d89505d4e7bc8e4076afb25db8a288b95
Alexander Ziskind <alex@nuvious.com>
2017-03-06 11:36:22 -0500

    Removed status bar util android setting View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION as it was interfering with a grid layout

--- app/shared/status-bar-util.ts
+++ app/shared/status-bar-util.ts
@@ -27,7 +27,7 @@ export function setStatusBarColors() {
                 const decorView = window.getDecorView();
                 decorView.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             }
