4e7780b69e71fb30026568edbceb2c9be8f60214
Alexander Ziskind <alex@nuvious.com>
2017-03-17 08:01:08 -0400

    Extracted html into its own file from inc-dec.component

--- app/pt-backlog/shared/inc-dec.component.ts
+++ app/pt-backlog/shared/inc-dec.component.ts
@@ -4,23 +4,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 @Component({
     moduleId: module.id,
     selector: 'inc-dec',
-    template: `     <StackLayout orientation="horizontal" class="pm-wrapper">
-                        <StackLayout class="{{ decEnabled ? 'pm-left-wrapper' : 'pm-left-wrapper pm-wrapper-disabled' }}">
-                        <Button class="{{ decEnabled ? 'pm-left' : 'pm-left pm-disabled' }}" (tap)="decTap()" [isEnabled]="decEnabled">
-                            <FormattedString>
-                                <Span text="-" fontSize="26" ios:foregroundColor="{{ decEnabled ? 'gray' : 'white' }}" android:foregroundColor="white"></Span>
-                            </FormattedString>
-                        </Button>
-                        </StackLayout>
-                        <StackLayout class="{{ incEnabled ? 'pm-right-wrapper' : 'pm-right-wrapper pm-wrapper-disabled' }}">
-                        <Button class="{{ incEnabled ? 'pm-right' : 'pm-right pm-disabled' }}" (tap)="incTap()" [isEnabled]="incEnabled">
-                            <FormattedString>
-                                <Span text="+" fontSize="26" ios:foregroundColor="{{ incEnabled ? 'gray' : 'white' }}" android:foregroundColor="white"></Span>
-                            </FormattedString>
-                        </Button>
-                        </StackLayout>
-                    </StackLayout>
-                    `,
+    templateUrl: 'inc-dec.component.html',
     styleUrls: ['inc-dec.component.css']
 })
 export class IncDecComponent {
